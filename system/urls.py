4c5f37a0540fd81811c989e6ee5d14d9d9ede0f2
zzq <13041129187@163.com>
2022-05-10 16:27:04 +0800

    updata 20220510

--- system/urls.py
+++ system/urls.py
@@ -9,7 +9,11 @@ urlpatterns = [
     path('sysconfig.html', views.system_config),
     path('usrconfig.html', views.usr_config),
     path('newusr.html', views.new_usr),
-    path('sendData',views.send_data)
+    path('sendData',views.send_data),
+    path('heartbeat',views.heartbeat),
+    path('searchmid.html',views.search_mid),
+    path('searchtime.html',views.search_time),
+    path('audiofile',views.audiofile)
 
 
 ]
--- system/views.py
+++ system/views.py
@@ -1,18 +1,47 @@
 from django.shortcuts import render
 from django.http import JsonResponse,HttpResponseRedirect
 from .forms import SysconfigForm,UsrForm
-import os,sqlite3,hashlib
+import os,sqlite3,hashlib,json,threading
+import time,datetime
 from . import udp
 
-data = {"cmdCheck": 0x02, "Seq": 0x15,
-        "audioPara": {"audioFunc": 0, "audioType": 1, "recordType": 1, "recordTime": 2, "nDevNo": 0, "nCapNo": 0,
-                      "mp3Bps": 128, "sampleRate": 48000, "timeInterval": 10, "fileName": "new.mp3",
-                      "isStereoSaveFlag": 0}}
-udp.senddata(data)
-res = udp.getdata(data)
-print(res["audioAckValue"]["funcResult"])
-
 # Create your views here.
+# data = {"cmdCheck": 0x02, "Seq": 0x15,
+#         "audioPara": {"audioFunc": 0, "audioType": 1, "recordType": 0, "recordTime": 20, "nDevNo": 1, "nCapNo": 0,
+#                       "mp3Bps": 128, "sampleRate": 48000, "timeInterval": 10, "fileName": "new.mp3",
+#                       "isStereoSaveFlag": 0}}
+# udp.senddata(data)
+# res = udp.getdata(data)
+# print(res["audioAckValue"]["funcResult"])
+
+def mkdir():
+    threading.Timer(43200,mkdir).start()
+    current_time = time.strftime("%Y-%m-%d", time.localtime())
+    now_time = datetime.datetime.now()
+    lastday_time = (now_time + datetime.timedelta(days=+1)).strftime("%Y-%m-%d")
+    path = 'static/record/'+current_time
+    if not os.path.exists(path):
+        os.mkdir(path)
+        os.mkdir(path+'/1')
+        os.mkdir(path+'/2')
+        os.mkdir(path+'/3')
+        os.mkdir(path+'/4')
+    else:
+        pass
+    path1 = 'static/record/'+lastday_time
+    if not os.path.exists(path1):
+        os.mkdir(path1)
+        os.mkdir(path1+'/1')
+        os.mkdir(path1+'/2')
+        os.mkdir(path1+'/3')
+        os.mkdir(path1+'/4')
+    else:
+        pass
+    print(lastday_time)
+
+mkdir()
+
+
 def main(request):
 
     if request.method == 'POST':
@@ -99,10 +128,183 @@ def dele_usr():
 
         return render(request, 'system/usrconfig.html',{'form': form,'name':name,'permiss':permiss})
 
-def send_data(request):
+def search_mid(request):
+    if request.method == 'POST':
+        lists = []
+        start = request.POST['start']
+        start_date = datetime.datetime.strptime(start, '%Y-%m-%dT%H:%M').strftime('%Y-%m-%d')
 
-    data = {"cmdCheck":0x02,"Seq":0x15,"audioPara":{"audioFunc":1,"audioType":1,"recordType":1,"recordTime":2,"nDevNo":0,"nCapNo":0,
-                                                    "mp3Bps":128,"sampleRate":48000,"timeInterval":10,"fileName":"new.mp3","isStereoSaveFlag":0}}
+        start_time = datetime.datetime.strptime(start, '%Y-%m-%dT%H:%M').strftime('%H-%M-00')
+        end = request.POST['end']
+        end_date = datetime.datetime.strptime(end, '%Y-%m-%dT%H:%M').strftime('%Y-%m-%d')
+        end_time = datetime.datetime.strptime(end, '%Y-%m-%dT%H:%M').strftime('%H-%M-00')
+        channel_no = request.POST['no']
+
+        if channel_no == 'all':
+            path0 = 'static/record/' + start_date
+            if not os.path.exists(path0):
+                pass
+            else:
+                i = 1
+
+                while i < 5:
+                    path = 'static/record/' + start_date + '/' + str(i)
+                    if not os.listdir(path):
+                        pass
+
+                    else:
+                        lists.append(start_date+ '/' + str(i))
+                    i = i + 1
+
+
+        else:
+            path0 = 'static/record/' + start_date
+            if not os.path.exists(path0):
+                pass
+            else:
+                path = 'static/record/' + start_date + '/' + channel_no
+                if not os.listdir(path):
+                    pass
+
+
+                else:
+                    lists.append(start_date + '/' + channel_no)
+
+        datestart = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+        dateend = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+        while datestart < dateend:
+            datestart += datetime.timedelta(days=1)
+            startdate = datetime.datetime.strftime(datestart, '%Y-%m-%d')
+            if channel_no == 'all':
+                path0 = 'static/record/' + startdate
+                if not os.path.exists(path0):
+                    pass
+                else:
+                    i = 1
+
+                    while i < 5:
+                        path = 'static/record/' + startdate + '/' + str(i)
+                        if not os.listdir(path):
+                            pass
+
+
+                        else:
+                            lists.append(startdate+ '/' + str(i))
+                        i = i + 1
+
+
+            else:
+                path0 = 'static/record/' + startdate
+                if not os.path.exists(path0):
+                    pass
+                else:
+                    path = 'static/record/' + startdate + '/' + channel_no
+                    if not os.listdir(path):
+                        pass
+
+                    else:
+                        lists.append(startdate + '/' + channel_no)
+
+        print(lists)
+
+        return render(request, 'system/searchmid.html',
+                      {'lists': lists, 'start_a': start, 'end_a': end, 'channel_no_a': channel_no})
+
+    else:
+
+        return render(request, 'system/searchmid.html')
+
+
+def search_time(request):
+    lists = []
+    start = request.POST['start']
+    start_date = datetime.datetime.strptime(start,'%Y-%m-%dT%H:%M').strftime('%Y-%m-%d')
+
+    start_time = datetime.datetime.strptime(start,'%Y-%m-%dT%H:%M').strftime('%H-%M-00')
+    end = request.POST['end']
+    end_date = datetime.datetime.strptime(end,'%Y-%m-%dT%H:%M').strftime('%Y-%m-%d')
+    end_time = datetime.datetime.strptime(end,'%Y-%m-%dT%H:%M').strftime('%H-%M-00')
+    channel_no = request.POST['no']
+
+    if channel_no=='all':
+        path0 = 'static/record/' + start_date
+        if not os.path.exists(path0):
+            pass
+        else:
+            i=1
+            mark = False
+            while i<5:
+                path = 'static/record/' + start_date + '/' + str(i)
+                if not os.listdir(path):
+                    pass
+
+                else:
+                    mark = True
+                i = i + 1
+            if mark:
+                lists.append(start_date)
+    else:
+        path0 = 'static/record/' + start_date
+        if not os.path.exists(path0):
+            pass
+        else:
+            path = 'static/record/' + start_date + '/' + channel_no
+            if not os.listdir(path):
+                pass
+
+
+            else:
+                lists.append(start_date+ '/' + channel_no)
+
+    datestart = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    dateend = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+    while datestart < dateend:
+        datestart += datetime.timedelta(days=1)
+        startdate = datetime.datetime.strftime(datestart,'%Y-%m-%d')
+        if channel_no == 'all':
+            path0 = 'static/record/' + startdate
+            if not os.path.exists(path0):
+                pass
+            else:
+                i = 1
+                mark = False
+                while i < 5:
+                    path = 'static/record/' + startdate + '/' + str(i)
+                    if not os.listdir(path):
+                        pass
+
+
+                    else:
+                        mark = True
+                    i = i + 1
+                if mark:
+                    lists.append(startdate)
+        else:
+            path0 = 'static/record/' + startdate
+            if not os.path.exists(path0):
+                pass
+            else:
+                path = 'static/record/' + startdate + '/' + channel_no
+                if not os.listdir(path):
+                    pass
+
+                else:
+                    lists.append(startdate + '/' + channel_no)
+
+
+    print(lists)
+
+
+    return render(request, 'system/searchtime.html',
+                      {'lists': lists, 'start_a': start, 'end_a': end, 'channel_no_a': channel_no})
+
+
+
+def send_data(request):
+    data =json.loads(request.POST['mes'])
+    print(data)
+
+
     udp.senddata(data)
     res = udp.getdata(data)
     if res["audioAckValue"]["funcResult"]==0:
@@ -110,5 +312,19 @@ def send_data(request):
         return JsonResponse({ 'msg': 'success'})
     else:
         return JsonResponse({'msg': 'failed'})
+
+def heartbeat(request):
+    res = udp.heartbeat()
+    if res==0:
+        return JsonResponse({'msg': 'failed'})
+
+    return JsonResponse({'msg': 'success'})
+
+def audiofile(request):
+    name = request.GET.get('name', default='10000000')
+    path = 'static/record/' + name
+    list = os.listdir(path)
+    print(list)
+    return render(request, 'system/audiofile.html',{'list':list})
 
 
